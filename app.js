be660b32a342679eed2395f239b2d844eaae482d
Pawan Deep Naugain <pawan.officeal@gmail.com>
2024-02-11 19:52:45 -0600

    commit 1

---
+++ app.js
@@ -0,0 +1,5 @@
+let button = document.querySelector('.btn1');
+let body = document.querySelector('.main');
+button.addEventListener('click', ()=>{
+    body.classList.add('changeBGbody');
+})
---
+++ pawan.py
@@ -0,0 +1,24 @@
+
+a = 10
+b = 2
+if a > b:
+ print("a is greater than b")
+else:
+ print("a is not greater than b")
+
+""" 2nd """
+
+a = b+1
+b = 3
+c = a+1
+if a>b:
+ print("a is graete then b!!!")
+else:
+ print("different answer")
+
+""" # 3 """
+a1 = int(10)
+a2 = str(10)
+print(a1)
+print(a2)
+
